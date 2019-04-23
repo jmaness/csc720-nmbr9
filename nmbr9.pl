5cfc13c36faeac89489809ef834c97e70b19aca7
Jeremy Maness <jwmaness@gmail.com>
2019-04-23 16:02:16 -0400

    More cleanup

--- nmbr9.pl
+++ nmbr9.pl
@@ -4,7 +4,7 @@
 
 
 % Adapted from "The Art of Prolog" (Sterling and Shapiro, p. 401.)
-%
+% for organizing game play
 play(Heuristic) :-
     initialize(Position),
     write('Game = '),write(Position),
@@ -53,14 +53,12 @@ new_deck(D) :- random_permutation(
 
 draw_card(game([C | New_Deck], _, Revealed_Cards, Board), game(New_Deck, C, [C | Revealed_Cards], Board)).
 
-
+% The game is over when all cards of the deck have been revealed.
 game_over(game([], _, _, board(Tiles)), Result) :-
     score(Tiles, Result).
 
 % Draw board
-display_game(game(_, _, _, board([]))) :-
-    writeln('').
-
+display_game(game(_, _, _, board([]))) :- writeln('').
 display_game(game(_, card(_, Card_Value), _, Board)) :-
     Board = board(Tiles),
     x_min(Tiles, X_min),
@@ -76,10 +74,12 @@ display_game(game(_, card(_, Card_Value), _, Board)) :-
 
 print_levels(board(Tiles), X_min, X_max, Y_min, Y_max, Z, Z) :-
     level_tiles(Tiles, Z, Level_tiles),
+    write('Level = '),writeln(Z),nl,nl,
     print_level(Level_tiles, X_min, X_max, Y_min, Y_max), !.
 
 print_levels(Board, X_min, X_max, Y_min, Y_max, Z_min, Z_max) :-
-    Board = board(Tiles),
+    Board = board(Tiles), 
+    write('Level = '),writeln(Z_max),nl,nl,
     level_tiles(Tiles, Z_max, Level_tiles),
     print_level(Level_tiles, X_min, X_max, Y_min, Y_max),
     Z is Z_max - 1,
@@ -108,8 +108,8 @@ print_row(Level_tiles, Board_x_min, Board_x_max, Y) :-
 
 print_cell(Level_tiles, X, Y) :-
     (find_tile(Level_tiles, X, Y, Tile) ->
-         tile_id(Tile, TileId),
-         Value is TileId mod 10,
+         tile_id(Tile, Tile_id),
+         Value is Tile_id mod 10,
          write(Value);
      write(" ")).
 
@@ -161,13 +161,17 @@ move_with_max_level([M1, S1, A1], [M2, S2, A2], [M, S, A]) :-
       move_with_max_score([M1, S1, A1], [M2, S2, A2], [M, S, A]))).
 
 
-make_tile(Id, Points, tile(Id, 0, Points)).
-make_tile(Id, Level, Points, tile(Id, Level, Points)).
-
-
-tile_xy_coords(tile(_, _, Points), Points).
-tile_xyz_coords(tile(_, Z, Points), Coords) :- maplist(\P^xyz_coords(P, Z), Points, Coords).
-xyz_coords([X, Y], Z, [X, Y, Z]).
+% Card rules
+card(_, 0, Points) :- zero(Points).
+card(_, 1, Points) :- one(Points).
+card(_, 2, Points) :- two(Points).
+card(_, 3, Points) :- three(Points).
+card(_, 4, Points) :- four(Points).
+card(_, 5, Points) :- five(Points).
+card(_, 6, Points) :- six(Points).
+card(_, 7, Points) :- seven(Points).
+card(_, 8, Points) :- eight(Points).
+card(_, 9, Points) :- nine(Points).
 
 % Tile templates
 zero([[0,0], [1,0], [2,0], [0,1], [2,1], [0,2], [2, 2], [0,3], [1,3], [2,3]]).
@@ -181,17 +185,13 @@ seven([[0,0], [0,1], [1,1], [1,2], [0,3], [1,3], [2,3]]).
 eight([[0,0], [1,0], [0,1], [1,1], [1,2], [2,2], [1,3], [2,3]]).
 nine([[0,0], [1,0], [0,1], [1,1], [0,2], [1,2], [2,2], [0,3], [1,3], [2,3]]).
 
-card(_, 0, Points) :- zero(Points).
-card(_, 1, Points) :- one(Points).
-card(_, 2, Points) :- two(Points).
-card(_, 3, Points) :- three(Points).
-card(_, 4, Points) :- four(Points).
-card(_, 5, Points) :- five(Points).
-card(_, 6, Points) :- six(Points).
-card(_, 7, Points) :- seven(Points).
-card(_, 8, Points) :- eight(Points).
-card(_, 9, Points) :- nine(Points).
-card(_, _, _) :- !, fail.
+% Tile rules
+make_tile(Id, Points, tile(Id, 0, Points)).
+make_tile(Id, Level, Points, tile(Id, Level, Points)).
+
+tile_xy_coords(tile(_, _, Points), Points).
+tile_xyz_coords(tile(_, Z, Points), Coords) :- maplist(\P^xyz_coords(P, Z), Points, Coords).
+xyz_coords([X, Y], Z, [X, Y, Z]).
 
 getX([X, _], X).
 getY([_, Y], Y).
@@ -253,7 +253,7 @@ move(game(_, card(Card_id, Card_value), _, board([])), tileMove(Tile, 0)) :-
 move(game(_, card(Card_id, Card_value), _, board(Tiles)), Move) :-
     card(Card_id, Card_value, Points),
     make_tile(Card_id, Points, Tile),
-    nextMoveBounds(Tiles, Bounds),
+    move_bounds(Tiles, Bounds),
     (X_min, X_max, Y_min, Y_max, Z_min, Z_max) = Bounds,
     X in X_min..X_max,
     Y in Y_min..Y_max,
@@ -266,7 +266,9 @@ move(game(_, card(Card_id, Card_value), _, board(Tiles)), Move) :-
     adjacent(Translated_tile, Tiles, Move),
     label([X, Y, Z, R]).
 
-nextMoveBounds(Tiles, Bounds) :-
+% The move bounds are based on the x, y, z ranges of the tiles on the board plus
+% the maximum x,y dimension of all possible tiles (4 units) and one additional z level
+move_bounds(Tiles, Bounds) :-
     x_min(Tiles, X_min),
     x_max(Tiles, X_max),
     y_min(Tiles, Y_min),
@@ -284,18 +286,18 @@ nextMoveBounds(Tiles, Bounds) :-
 
 % Rotates tile T counter-clockwise N times.
 rotate(0, Tile, Tile) :- !.
-rotate(N, tile(Id, Z, Tile_points), Rotated_tile) :-
+rotate(N, tile(Id, Z, Points), Rotated_tile) :-
     N > 0,
     N1 is N - 1,
-    maplist(rotate_coord, Tile_points, Q),
+    maplist(rotate_point, Points, Q),
     make_tile(Id, Z, Q, M),
     rotate(N1, M, Rotated_tile).
 
-
-rotate_coord([A,B], [C,D]) :-
+rotate_point([A,B], [C,D]) :-
     C is -1 * B,
     D is A.
 
+% Translate a tile in three dimensions restricted by the specified bonds
 translate(tile(Id, Level, Points), X, Y, Z, Bounds, tile(Id, Z1, Translated_points)) :-
     Z1 is Level + Z,
     (_, _, _, _, Z_min, Z_max) = Bounds,
@@ -312,6 +314,8 @@ translate_point([Px, Py], X, Y, Bounds, [Px2, Py2]) :-
     Py2 >= Y_min,
     Py2 =< Y_max.
 
+% A tile is disjoint from a set of tiles if no tile points are the same as any
+% points of the set of tiles.
 disjoint(Tiles, Tile) :-
     tile_z(Tile, Z),
     level_tiles(Tiles, Z, Level_tiles),
@@ -320,7 +324,7 @@ disjoint(Tiles, Tile) :-
     intersection(TileXY_coords, LevelXY_coords, CommonCoords),
     length(CommonCoords, 0).
 
-unionTileCoords(Acc, Tile, Res) :-
+union_tile_coords(Acc, Tile, Res) :-
     tile_xyz_coords(Tile, Coords),
     union(Acc, Coords, Res).
 
@@ -328,6 +332,11 @@ union_tile_xy_coords(Acc, Tile, Res) :-
     tile_xy_coords(Tile, Coords),
     union(Acc, Coords, Res).
 
+% For a valid move, a given tile must be:
+%    * adjacent to one or more tiles on the same level
+%    * completely supported by tiles on the preceding level
+%      (i.e. all tile points are adjacent to tile points directly below)
+%    * overlaps two or more tiles on the preceding level
 adjacent(Tile, Tiles, Move) :-
     tile_z(Tile, Z),
     level_tiles(Tiles, Z, Level_tiles),
@@ -365,13 +374,6 @@ overlaps_preceding_level(Tile, Level_tiles) :-
     subset(Tile_xy_coords, Level_xy_coords),
     overlaps_multiple_tiles(Tile, Level_tiles).
 
-% helper rules
-board_tile_xy_coords(Tiles, XY_coords) :-
-    foldl(\T^A^union_tile_xy_coords(A, T), Tiles, [], XY_coords).
-
-level_tiles(Tiles, Z, Level_tiles) :-
-    include(\T^(tile_z(T, Z)), Tiles, Level_tiles).
-
 overlaps_multiple_tiles(Tile, Tiles) :-
     overlapped_tiles(Tile, Tiles, Overlapped_tiles),
     maplist(\T^tile_id(T), Overlapped_tiles, Ids),
@@ -389,13 +391,18 @@ overlaps(Top_tile, Bottom_tile) :-
     length(Common_coords, L),
     L > 0.
 
+board_tile_xy_coords(Tiles, XY_coords) :-
+    foldl(\T^A^union_tile_xy_coords(A, T), Tiles, [], XY_coords).
+
+level_tiles(Tiles, Z, Level_tiles) :-
+    include(\T^(tile_z(T, Z)), Tiles, Level_tiles).
+
+% Computes the total score for the given list of tiles
 score(Tiles, Score) :-
     foldl(\T^A^sum_tile_score(T, A), Tiles, 0, Score).
 
-sum_tile_score(Tile, Acc, Score) :-
-    tile_id(Tile, TileId),
-    Value is TileId mod 10,
-    tile_z(Tile, Z),
+sum_tile_score(tile(Id, Z, _), Acc, Score) :-
+    Value is Id mod 10,
     Score is Acc + (Value * Z).
 
 
